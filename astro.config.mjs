1bc9679584e01c3e6f205bfa17bbc48e14a61bc2
miko <98265628+mikoprn@users.noreply.github.com>
2025-04-28 15:44:17 +0700

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig, passthroughImageService } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
+});
